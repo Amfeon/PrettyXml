67e20e8665d386bc04c79e7ca3a7f9abc7d5ac68
amfeon <amfeon@bk.ru>
2025-01-10 10:37:49 +0500

    adds demo

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,15 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
+
+const isGP_Build = process.env.APP_ENV === 'gp'
+
 export default defineConfig({
   plugins: [
     vue(),
     XMLLoader(),
   ],
-  build:{
+  build: isGP_Build ? {} : {
     lib:{
       entry: resolve(__dirname, "src/index.js"),      
       name: "pretty-xml-vue3",
