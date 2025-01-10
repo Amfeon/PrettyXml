042ec41daa5042afd321adc3e1ef247562011528
amfeon <amfeon@bk.ru>
2025-01-10 10:29:45 +0500

    add dist

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import PrettyXml from "./components/PrettyXml.vue"
-import './assets/base.css'
-// export default {
-//     install: (app, options) =>{
-//         app.component("PrettyXml", PrettyXml)
-//     }
-// }
-
-export {PrettyXml}
